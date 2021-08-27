07fa5ba8d7492bbed16b575e4d5b5f26c04d407f
alkanaft123@yandex.ru <alkanaft123@yandex.ru>
2021-08-28 00:52:31 +0300

    l1l1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Message from './components/message.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Message text='my text'/>
+    </div>
+  );
+}
+
+export default App;
