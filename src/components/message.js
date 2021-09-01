0af7967f4dac7aaed7ef6448ea3ce8690932995e
popapauka <gfavlg@yandex.ru>
2021-09-01 16:59:23 +0300

    l2
    l2

--- src/components/message.js
+++ src/components/message.js
@@ -1,3 +1,6 @@
-export default (props)=>{
-    return <span>{props.text}</span>
+export default (props) => {
+    return <div className={`wrp ${props.messageObj.author}`}>
+        <p className="user">{props.messageObj.author}</p>
+        <p className="text">{props.messageObj.text}</p>
+    </div>
 }
